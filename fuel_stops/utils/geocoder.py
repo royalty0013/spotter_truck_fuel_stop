f7eb168679617111e65a0710aea318614ba8f923
Braimah Hassan <royalty0013@gmail.com>
2025-06-24 15:41:44 +0100

    change geocoder fetch parameter title

--- fuel_stops/utils/geocoder.py
+++ fuel_stops/utils/geocoder.py
@@ -11,19 +11,19 @@ class Geocoder:
     def __init__(self):
         self.client = Nominatim(user_agent="fuelstop_geocoder")
 
-    def fetch(self, address: str) -> Optional[Tuple[float, float]]:
-        """Fetches the geocode for a given address.
+    def fetch(self, truckstop_name: str) -> Optional[Tuple[float, float]]:
+        """Fetches the geocode for a given truckstop_name.
 
         Args:
-            address (str): The address to geocode.
+            truckstop_name (str): The truckstop_name to geocode.
 
         Returns:
-            Optional[Tuple[float, float]]: The geocode for the given address, or None if the geocoding fails.
+            Optional[Tuple[float, float]]: The geocode for the given truckstop_name, or None if the geocoding fails.
         """
         try:
-            location = self.client.geocode(address)
+            location = self.client.geocode(truckstop_name)
             if location:
                 return location.Latitude, location.Longitude
         except (GeocoderTimedOut, GeocoderServiceError) as e:
-            logger.error(f"Geocoding service error for {address}: {e}")
+            logger.error(f"Geocoding service error for {truckstop_name}: {e}")
         return None
